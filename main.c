9142a8848228cd901afb20d67e8bc717b4f784dc
Konrad Mach <konrad.mach@t-online.de>
2019-01-20 07:28:19 +0100

    Update functions / ready to go Version / no library

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 /**
- *
- *
- *
- **/
+ *Summary: This is a library for time functions.
+ *date: 18.12.2018
+ *author: Konrad Mach
+ *last modified on/by : 20.01.2019/Konrad Mach
+  **/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,20 +12,14 @@
  *
  *
  **/
-// Globale variablen.
+// Global vars.
 
 int DAY;
 int MONTH;
 int YEAR;
 
-int day_of_the_year(int day, int month, int year)
-{
-
-    return 0;
-}
-
+/// Checks if given year is leapyear.
 int is_leapyear(int year){
-    // Hier auch möglich exists_date mti 01.01.year übergeben um nach Jahr zu prüfen.
     if(year < 1582 || year > 2400){
         return -1;
     }
@@ -42,29 +37,23 @@ int is_leapyear(int year){
         return 0;
 }
 
-void input_date(&day, &month, &year){
-
-do{
-    printf("\n");
-    printf("Bitte geben Sie den Tag ein: ");
-    scanf("%i", &day);
-    printf("\n Bitte geben Sie den Monat ein: ");
-    scanf("%i", & month);
-    printf("\n Bitte geben Sie das Jahr ein: ");
-    scanf("%i", &year);
-
-}while(exist_date(&day, &month, &year) == 0) // To-Do: Checken ob die übergabeparams gehen.
-
-}
-
+///returns days of month, needs month and year.
 int get_days_for_month(int month, int year){
     if(year < 1582 || year > 2400 || month < 1 || month > 12){
         return -1;
     }
 
+    if(month == 2){
+        if(is_leapyear(year) == 1){
+            return 29;
+        }
+        else{
+            return 28;
+        };
+    }
+
     switch (month){
         case 1: return 31; break;
-        case 2: return 28; break; // To-Do: Prüfung Schaltjahr
         case 3: return 31; break;
         case 4: return 30; break;
         case 5: return 31; break;
@@ -79,14 +68,57 @@ int get_days_for_month(int month, int year){
 return 0;
 }
 
+/// Checks if the given date is valid.
 int exist_date(int day, int month, int year){
-    if(year < 1582 || year > 2400){
+    if(year < 1582 || year > 2400 || month < 1 || month > 12){
         return -1;
     }
+
+    if(day > get_days_for_month(month,year) || day < 1){
+        return -1;
+    }
+
+    return 0;
 }
+
+/// returns the days of the given year till an specific date.
+int day_of_the_year(int day, int month, int year)
+{
+    if(exist_date(day,month,year)){
+        return -1;
+    }
+
+    int totalDays = 0;
+    int currMonth = month -1;
+
+    for(int i = 0; i < (month - 1); i++ ){
+        totalDays += get_days_for_month(currMonth, year);
+        currMonth--;
+    }
+    totalDays += day;
+
+    return totalDays;
+}
+
+//void input_date(&day, &month, &year){
+//
+//do{
+//    printf("\n");
+//    printf("Bitte geben Sie den Tag ein: ");
+//    scanf("%i", &day);
+//    printf("\n Bitte geben Sie den Monat ein: ");
+//    scanf("%i", & month);
+//    printf("\n Bitte geben Sie das Jahr ein: ");
+//    scanf("%i", &year);
+//
+//}while(exist_date(&day, &month, &year) == 0) // To-Do: Checken ob die übergabeparams gehen.
+//
+//}
+
+
 
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+    printf("Tag des Jahres: %i\n", day_of_the_year(31, 2, 2018));
     return 0;
 }
